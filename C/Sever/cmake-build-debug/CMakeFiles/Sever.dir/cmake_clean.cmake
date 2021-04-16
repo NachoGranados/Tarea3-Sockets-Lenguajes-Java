e8be6fdbbd59f6597e980b8a6d8ef09fd5c272ce
NachoGranados <ignaciogramar@gmail.com>
2021-04-16 00:16:35 -0600

    C sockets

---
+++ C/Sever/cmake-build-debug/CMakeFiles/Sever.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sever.dir/main.c.o"
+  "Sever"
+  "Sever.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sever.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
